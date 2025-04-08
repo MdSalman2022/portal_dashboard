3f80cfbe63d14cb0380195fdc898f31a3795b21b
MdSalman2022 <mehedi.salman102@gmail.com>
2025-04-09 02:35:18 +0600

    fixed header

--- header.php
+++ header.php
@@ -47,7 +47,7 @@ if ($isLoggedIn && isset($_SESSION['role'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https:
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
         :root {
             --primary: #4361ee;
